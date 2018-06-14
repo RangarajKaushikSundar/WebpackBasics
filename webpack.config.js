fac290b6205c43b20e46210b4f81861ab71ab803
Rangaraj Kaushik Sundar <kaushik.rangaraj@gmail.com>
2018-06-13 22:48:20 -0700

    Added css loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,16 @@ module.exports = {
   entry: "./js/actions.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "build.js"
+    filename: "build.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader', // Loaders are loaded in reverse order. So CSS Loader is loaded first followed by style. Reversing the order will break.
+        'css-loader'
+      ]
+    }]
   }
 }
